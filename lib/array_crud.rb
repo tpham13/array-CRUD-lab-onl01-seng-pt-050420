08e5ef8c66fd8f851d0e99fe665a90ad87d260ce
Thanh Pham <tpham0123@gmail.com>
2020-05-11 02:38:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,10 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  a
+  array = [ ]
+   array << 1 
+  element = [ ]
+    element << a
 end
 
 def add_element_to_start_of_array(array, element)
