4f8e0a187bf999074edcc802f7560115477810c1
Thanh Pham <tpham0123@gmail.com>
2020-05-11 02:36:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element_to_start_of_array << new_array
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array << new_array
+  
 end
 
 def remove_element_from_end_of_array(array)
