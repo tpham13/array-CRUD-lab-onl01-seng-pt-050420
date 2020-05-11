c69a4757b0a944cc5a41741249836f77f10fe859
Thanh Pham <tpham0123@gmail.com>
2020-05-11 02:25:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push = ["cat"]
+  array = ["cat","dog","duck","horse"]
+    array << 
   element.push = ["dog"]
   
 end
